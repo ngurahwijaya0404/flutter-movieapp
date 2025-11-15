fa73fa16bf48ee5d0a460f229b4bfba572df6901
Ngurah Wijaya <ngurahwijaya0404@gmail.com>
2025-11-15 18:16:22 +0800

    Add TMDB constants configuration (API key & base URL) and Add Movie model class with JSON parser

---
+++ lib/config/constants.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  
+  static const String tmdbApiKey = "92f0da2b8f893b2c5a47230db00a9322";
+  static const String tmdbBaseUrl = "https://api.themoviedb.org/3/";
+}
---
+++ lib/models/movie.dart
@@ -0,0 +1,25 @@
+class Movie {
+  final int id;
+  final String title;
+  final String? posterPath;
+  final String overview;
+  final double rating;
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.posterPath,
+    required this.overview,
+    required this.rating,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      id: json['id'],
+      title: json['title'] ?? "",
+      posterPath: json['poster_path'],
+      overview: json['overview'] ?? "",
+      rating: (json['vote_average'] ?? 0).toDouble(),
+    );
+  }
+}
